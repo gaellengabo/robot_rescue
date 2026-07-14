2094fa5e1bb8b2c18101ea2463e22293cb6d8bdb
Gaelle <your.email@example.com>
2026-07-13 21:15:17 -0400

    removed tests

--- main.py
+++ main.py
@@ -10,14 +10,9 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
-
-#hiiiiii
-#hello!
-#test
 # Create your objects here.
 ev3 = EV3Brick()
 
 
 # Write your program here.
 ev3.speaker.beep()
-x = 2
