c264f618ae8fd787a16442e8de51a39be5c88ab7
ji0844 <joyceli0844@gmail.com>
2026-07-15 21:53:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import ColorSensor
 from pybricks.parameters import Port, Color
 from pybricks.tools import wait
+from pybricks import nxtdevices
 
 ev3 = EV3Brick()
 ev3.light.on(Color.ORANGE)
 
 # Make sure your sensor is physically plugged into Port 1!
-brightness = ColorSensor(Port.S1)
+brightness = nxtdevices.TouchSensor(Port.S1)
 
 while True:
     ev3.screen.print(brightness.reflection())
