fe46ec98fdee32e3fcdf670c5813020b31510550
ji0844 <joyceli0844@gmail.com>
2026-07-15 20:52:43 -0400

    Update light_testing.py

--- light_testing.py
+++ light_testing.py
@@ -3,5 +3,5 @@ from pybricks.ev3devices import Motor, LightSensor
 brightness = LightSensors(Port.A)
 
 while True:
-  screen.print(brightness)
+  screen.print(brightness.reflection())
   wait(1000)
