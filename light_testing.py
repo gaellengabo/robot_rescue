1af8dd84f071eab78e8fb34eb5b637353d71bd4a
Gaelle <your.email@example.com>
2026-07-15 21:29:53 -0400

    update

--- light_testing.py
+++ light_testing.py
@@ -1,4 +1,10 @@
-from pybricks.ev3devices import Motor, LightSensor
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port, Color
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+from pybricks.ev3devices import LightSensor
 
 brightness = LightSensor(Port.S1)
 
