909027ac36d2ab44172e5455a8e6f61461279bdf
Gaelle <your.email@example.com>
2026-07-15 21:38:55 -0400

    update light sensor code to print brightness every second

--- main.py
+++ main.py
@@ -4,13 +4,32 @@ from pybricks.ev3devices import Motor
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
-from pybricks.ev3devices import LightSensor
 
 ev3 = EV3Brick()
 ev3.light.on(Color.ORANGE)
+ev3.speaker.beep()
 
-brightness = LightSensor(Port.S1)
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.D)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=114)
 
-while True:
-  screen.print(brightness.reflection())
-  wait(1000)
+left_motor.run_target(200, 360)
+wait(500)
+
+right_motor.run_target(200, 360)
+wait(500)
+
+robot.straight(200)
+wait(500)
+
+robot.straight(-200)
+wait(500)
+
+robot.turn(90)
+wait(500)
+
+robot.turn(-90)
+wait(500)
+
+ev3.light.off()
+ev3.speaker.beep(frequency=800, duration=500)
