3a5c3900089e200b1cea4382e803485974b57fc8
Gaelle <your.email@example.com>
2026-07-15 21:06:48 -0400

    motor test

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
-#!/usr/bin/env pybricks-micropython
+#!/usr/bin/env pybricks-pycurry
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor
-from pybricks.parameters import Port, Stop
+from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
 
-# Initialize the EV3 brick
 ev3 = EV3Brick()
+ev3.light.on(Color.ORANGE)
+ev3.speaker.beep()
 
-# Initialize the motors
 left_motor = Motor(Port.A)
 right_motor = Motor(Port.D)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=114)
 
-# Define robot dimensions
-WHEEL_DIAMETER = 56
-AXLE_TRACK = 114
+left_motor.run_target(200, 360)
+wait(500)
 
-# Set up the DriveBase
-robot = DriveBase(left_motor, right_motor, wheel_diameter=WHEEL_DIAMETER, axle_track=AXLE_TRACK)
+right_motor.run_target(200, 360)
+wait(500)
 
-ev3.speaker.beep(frequency=1000, duration=200)
+robot.straight(200)
+wait(500)
 
-# Drive forward 500 mm, then stop
-robot.straight(500)
-robot.stop(Stop.BRAKE)
+robot.straight(-200)
+wait(500)
 
-# Wait for 1 second
-wait(1000)
-
-# Turn 90 degrees to the right
 robot.turn(90)
-robot.stop(Stop.BRAKE)
+wait(500)
+
+robot.turn(-90)
+wait(500)
+
+ev3.light.off()
+ev3.speaker.beep(frequency=800, duration=500)
