6c962fbbf5f8020cbe2b2c67474b082cb94c6cdf
Gaelle <your.email@example.com>
2026-07-15 21:46:11 -0400

    update

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port, Color
-from pybricks.robotics import DriveBase
-from pybricks.tools import wait
 from pybricks.ev3devices import ColorSensor
+from pybricks.parameters import Port, Color
+from pybricks.tools import wait
 
 ev3 = EV3Brick()
 ev3.light.on(Color.ORANGE)
 
+# Make sure your sensor is physically plugged into Port 1!
 brightness = ColorSensor(Port.S1)
 
 while True:
-  screen.print(brightness.reflection())
-  wait(1000)
+    ev3.screen.print(brightness.reflection())
+    wait(1000)
