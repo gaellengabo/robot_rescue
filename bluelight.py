064f8ead7a3a94aa434a83c090b8c55c51b7b846
ji0844 <joyceli0844@gmail.com>
2026-07-18 14:36:22 -0400

    Update bluelight.py

--- bluelight.py
+++ bluelight.py
@@ -4,10 +4,11 @@ from pybricks.ev3devices import Motor
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
-from pybricks.ev3devices import LightSensor
+from pybricks.ev3devices import ColorSensor
 
 color = ColorSensor(Port.S4)
 
 while True:
-  screen.print(color.rgb())
-  wait(1000)
+  ev3.screen.clear()
+  ev3.screen.print(color.rgb())
+  wait(500)
