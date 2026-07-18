1d1c77c4abd9729dbbda9ea10d85844899d5c869
eshakripalani-creator <esha.kripalani@gmail.com>
2026-07-18 14:30:54 -0400

    Update bluelight.py

---
+++ bluelight.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port, Color
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+from pybricks.ev3devices import LightSensor
+
+brightness = LightSensor(Port.S1)
+
+while True:
+  screen.print(brightness.reflection())
+  wait(1000)
