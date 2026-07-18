01cc2f3c29b73f608039e9f5b1fea4db23d6ad3e
Gaelle <your.email@example.com>
2026-07-18 13:46:31 -0400

    update

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ BLACK_VALUE = 7
 WHITE_VALUE = 25     
 SETPOINT = (BLACK_VALUE + WHITE_VALUE) / 2
 
-KP = 2.0             
-BASE_SPEED = 100
+KP = 2.5             
+BASE_SPEED = 65
 
 while True:
     brightness = line_sensor.reflection()
