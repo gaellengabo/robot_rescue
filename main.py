cfd3dbae31666d6950e3fd5fee20470d21797d94
Gaelle <your.email@example.com>
2026-07-19 13:18:06 -0400

    update with color sensing for blue and sharpt turn

--- main.py
+++ main.py
@@ -1,46 +1,56 @@
-
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
-from pybricks import nxtdevices
 
 ev3 = EV3Brick()
 ev3.light.on(Color.ORANGE)
 
 left_motor = Motor(Port.A)   
 right_motor = Motor(Port.D)  
-line_sensor = nxtdevices.LightSensor(Port.S1)
-color_sensor = ev3devices.ColorSensor(Port.S4)
+
+line_sensor = ColorSensor(Port.S1)
+color_sensor = ColorSensor(Port.S4)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=114)
 
 BLACK_VALUE = 7     
 WHITE_VALUE = 25
-BLUE_R = 0
-BLUE_B = 100
-BLUE_G = 0
 SETPOINT = (BLACK_VALUE + WHITE_VALUE) / 2
+
+BLUE_R = 0
+BLUE_G = 0
+BLUE_B = 100
 
 KP = 3.1             
 BASE_SPEED = 56
 
 while True:
     brightness = line_sensor.reflection()
-    color = color_sensor.rgb()
+    r, g, b = color_sensor.rgb()
 
     ev3.screen.clear()
     ev3.screen.print(brightness)
 
-    # Proportional error formula from worksheet
+    # Blue Marker Check
+    if b > 40 and b > r and b > g: 
+        robot.stop()
+        ev3.speaker.beep(frequency=800, duration=250)
+        print("Blue detected! RGB: ({}, {}, {})".format(r, g, b))
+        robot.straight(20) # Move past marker slightly to prevent double-beeping
+
+    # Sharp Curve Recovery
+    if brightness >= 24:
+        ev3.speaker.beep(frequency=400, duration=100)
+        robot.drive(0, 120) 
+        wait(100) # Give the robot a moment to swing back toward the black line
+        continue
+
+    # Normal Line Following
     error = SETPOINT - brightness
-    
-    # Calculate turn speed based on how far off the line edge the robot is
     turn_rate = KP * error
-    
-    # Execute movement
     robot.drive(BASE_SPEED, turn_rate)
     
     wait(10)
