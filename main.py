9cad235733621a41cc7940740c7cd2d324dbeb0f
Gaelle <your.email@example.com>
2026-07-19 14:53:43 -0400

    update

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ color_sensor = ColorSensor(Port.S4)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=114)
 
-BLACK_VALUE = 3    
-WHITE_VALUE = 27
+BLACK_VALUE = 0    
+WHITE_VALUE = 33
 SETPOINT = (BLACK_VALUE + WHITE_VALUE) / 2
 
-KP = 3.5             
-BASE_SPEED = 50
+KP = 7.0             
+BASE_SPEED = 45
 
 RED_MIN = 3
 RED_MAX = 5
