1551d6e6001442af9b70dc0e3076517ff12962c3
ji0844 <joyceli0844@gmail.com>
2026-07-19 13:38:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
+from pybricks import nxtdevices
 
 ev3 = EV3Brick()
 ev3.light.on(Color.ORANGE)
@@ -11,7 +12,7 @@ ev3.light.on(Color.ORANGE)
 left_motor = Motor(Port.A)   
 right_motor = Motor(Port.D)  
 
-line_sensor = ColorSensor(Port.S1)
+line_sensor = nxtdevices.LightSensor(Port.S1)
 color_sensor = ColorSensor(Port.S4)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=114)
@@ -23,24 +24,25 @@ SETPOINT = (BLACK_VALUE + WHITE_VALUE) / 2
 KP = 3.1             
 BASE_SPEED = 56
 
+RED_MAX = 5
+RED_MIN = 3
+GREEN_MAX 5
+GREEN_MIN = 3
+BLUE_MAX = 16
+BLUE_MIN = 13
+
 while True:
-    brightness = line_sensor.reflection()
-    r, g, b = color_sensor.rgb()
+    brightness = line_sensor.ambient()
+    color = color_sensor.rgb()
 
     ev3.screen.clear()
     ev3.screen.print(brightness)
 
-    if b >= 9 and r <= 6 and g <= 6: 
+    if color[0] >= RED_MIN and color[0] <= RED_MAX and color[1] >= GREEN_MIN and color[1] <= GREEN_MAX and color[2] >= BLUE_MIN and color[2] <= BLUE_MAX: 
         ev3.speaker.beep(frequency=800, duration=150)
-        print("Blue detected! RGB: ({}, {}, {})".format(r, g, b))
+        print(f"Blue detected! {color}")
 
-    if brightness >= 24:
-        ev3.speaker.beep(frequency=400, duration=100)
-        robot.drive(0, 120) 
-        wait(100) 
-        continue
-
-    # norml line following
+    # normal line following
     error = SETPOINT - brightness
     turn_rate = KP * error
     robot.drive(BASE_SPEED, turn_rate)
