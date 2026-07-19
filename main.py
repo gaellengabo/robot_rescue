f7e092da9b555090958bc539a96e35d33842108a
Gaelle <your.email@example.com>
2026-07-19 14:00:43 -0400

    update

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ while True:
     ev3.screen.clear()
     ev3.screen.print(brightness)
 
-   if color[0] >= RED_MIN and color[0] <= RED_MAX and color[1] >= GREEN_MIN and color[1] <= GREEN_MAX and color[2] >= BLUE_MIN and color[2] <= BLUE_MAX:
+  if color[0] >= RED_MIN and color[0] <= RED_MAX and color[1] >= GREEN_MIN and color[1] <= GREEN_MAX and color[2] >= BLUE_MIN and color[2] <= BLUE_MAX:
         ev3.speaker.beep(frequency=800, duration=150)
         print("Blue detected! {}".format(color))
 
