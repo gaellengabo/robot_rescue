9fe68654089ee43ce47b4788419378ba293f681b
lillian-fischbeck <lily.fischbeck@gmail.com>
2026-07-20 20:30:17 -0400

    Adjust color detection thresholds and messages

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor
-from pybricks.parameters import Port, Color
+from pybricks.parameters import Port, Color, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
 from pybricks import nxtdevices
@@ -18,18 +18,18 @@ color_sensor = ColorSensor(Port.S4)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=114)
 
 BLACK_VALUE = 0    
-WHITE_VALUE = 49
+WHITE_VALUE = 35
 SETPOINT = (BLACK_VALUE + WHITE_VALUE) / 2
 
 KP = 3.0        
 BASE_SPEED = 27
 
-RED_MIN = 3
-RED_MAX = 5
-GREEN_MIN = 3
-GREEN_MAX = 5
-BLUE_MIN = 13
-BLUE_MAX = 16
+RED_MIN = 6
+RED_MAX = 8
+GREEN_MIN = 1
+GREEN_MAX = 3
+BLUE_MIN = 0
+BLUE_MAX = 2
 
 while True:
     brightness = line_sensor.reflection()
@@ -40,7 +40,9 @@ while True:
 
     if color[0] >= RED_MIN and color[0] <= RED_MAX and color[1] >= GREEN_MIN and color[1] <= GREEN_MAX and color[2] >= BLUE_MIN and color[2] <= BLUE_MAX:
         ev3.speaker.beep(frequency=800, duration=150)
-        print("Blue detected! {}".format(color))
+        print("Yay eve! {}".format(color))
+        break
+
 
     # normal line following
     error = SETPOINT - brightness
@@ -48,3 +50,5 @@ while True:
     robot.drive(BASE_SPEED, turn_rate)
     
     wait(10)
+
+robot.stop(Stop.BRAKE)
