73ab6d86fb92b920c61745027344a2c91c49d2e9
Gaelle <your.email@example.com>
2026-07-22 20:07:52 -0400

    starter code

---
+++ BattleBots/mainbattle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+ev3 = EV3Brick()
+
+ev3.light.on(Color.YELLOW)
+while Button.CENTER not in ev3.buttons.pressed():
+    wait(10)
+ev3.light.on(Color.GREEN)
+ev3.speaker.beep()
+wait(5000)
+
+
+#PUT YOUR CODE HERE  Delete mine
+
+ev3.light.on(Color.RED)
+wait(5000)
+This topic is closed for comments.
